4d3dbdb84a1f83f907f4b9997ab73502928cc599
Axel <axesan917@gmail.com>
2023-12-31 13:08:49 -0600

    corrección de el scroll en la imagen principal

--- js/script.js
+++ js/script.js
@@ -1,3 +1,8 @@
+document.addEventListener("scroll", function () {
+    let scrollPosition = window.scrollY;
+    document.querySelector('.principal-image img').style.transform = `translateY(${scrollPosition * 0.7}px)`;
+});
+
 let lastScrollTop = 0;
 let headerHidden = false;
 
